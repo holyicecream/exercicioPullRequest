f32fa370d1d5a252e3ef0245f1b28e28887a417c
Thomas <thomas.blendsbr@gmail.com>
2023-04-15 10:46:41 -0300

    javascript da calc
    código js para calculadora

---
+++ script.js
@@ -0,0 +1,15 @@
+let result = document.getElementById("result");
+
+
+function getResult() {
+  result.value = eval(result.value);
+}
+
+function calculate(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = "";
+
+}
